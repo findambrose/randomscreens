d6fa484ed213b0d59a0bb5883c2087cf7ee16276
findambrose <ambkyusya@gmail.com>
2020-10-29 14:50:38 +0300

    A couple edits

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:randomscreens/screens/Expenses.dart';
-import 'package:randomscreens/screens/Home.dart';
-// import 'package:randomscreens/screens/Cards.dart';
 import 'package:randomscreens/screens/Transfer.dart';
 
 void main() => runApp(MyApp());
@@ -12,17 +9,9 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      title: 'Flutter Demo',
+      title: 'Random Screens by Flutter',
       theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
+    
         primarySwatch: Colors.blue,
       ),
       home: Transfer(),
@@ -30,8 +19,6 @@ class MyApp extends StatelessWidget {
       //   '/next': (context) => Home(),
 
       // },
-
     );
   }
 }
-
